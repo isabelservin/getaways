ad9fba88fb521cc2e0d6fce7766aa8e7aaa27212
Roman Penn <pnnroman92@gmail.com>
2022-02-14 16:00:56 -0600

    got db routes up

--- server/models/Listings.js
+++ server/models/Listings.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 //create a mongoose schema
 const listingsSchema = mongoose.Schema({
-  address: String,
-  price: Number,
-  img: String,
-  description: String,
-  propertyType: String,
+  propertyId: { type: String, required: true },
+  address: { type: String, required: true },
+  price: { type: Number, required: true },
+  img: { type: String, required: true },
+  description: { type: String, required: true },
+  propertyType: { type: String, required: true },
 });
+
+const Listings = mongoose.model("Listings", listingsSchema);
 
 export default Listings;
