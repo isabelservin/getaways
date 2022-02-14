630cdfb7c48f21939a890fece353043c9a265265
Isabel Servin <isabel.servin@outlook.com>
2022-02-14 11:02:28 -0800

    initial setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+
+//initialize app
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.use(cors())
+
+const connectionStg = "mongodb+srv://team3:team321@cluster0.rzkvv.mongodb.net/myGetaways?retryWrites=true&w=majority"
+
+const port = process.env.PORT || 8080
+
+mongoose.connect(connectionStg)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server running on port: ${port}`)
+        })
+    })
+    .catch(err => { console.error(err) })
