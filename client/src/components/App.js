b5388acc7bea84dfe8649758f8ff9a1e19c542cd
Roman Penn <pnnroman92@gmail.com>
2022-02-16 14:43:44 -0600

    got authorization

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,25 +2,18 @@ import React from "react";
 import Landing from "./Landing";
 import UserForm from "./UserForm";
 import ListingsForm from "./ListingsForm";
+import Profile from "./Profile";
 import NavBar from "./NavBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  // return (
-  //   <div id="main-container">
-  //     <h3 id="site-name">Grand Getaways</h3>
-  //     <img id="landing-img" src={travel} alt="suitcases" />
-  //     <Listings />
-  //   </div>
-
-  // );
-
   return (
     <>
       <NavBar />
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/users" element={<UserForm />} />
           <Route path="/listingform" element={<ListingsForm />} />
         </Routes>
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <>
+  <Auth0Provider
+    domain="dev-ejvpqz-k.us.auth0.com"
+    clientId="9tAS6aIBAR40GqZgLV6FVToE340sCGMr"
+    redirectUri="http://localhost:3000/users"
+  >
     <App />
-  </>,
+  </Auth0Provider>,
   document.getElementById("root")
 );
