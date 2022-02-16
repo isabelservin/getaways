ee44358dc289d4eedc48b2941c63f1c9c2b56ee4
Roman Penn <pnnroman92@gmail.com>
2022-02-16 13:12:26 -0600

    moved most imports and exports to App.js and fixed select options

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import travel from "../images/travel.jpg";
+import Listings from "./Listings";
+
+const Landing = () => {
+  return (
+    <div id="main-container">
+      <h3 id="site-name">Grand Getaways</h3>
+      <img id="landing-img" src={travel} alt="suitcases" />
+      <Listings />
+    </div>
+  );
+};
+
+export default Landing;
--- client/src/index.js
+++ client/src/index.js
@@ -2,23 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import UserForm from "./components/UserForm";
-import ListingsForm from "./components/ListingsForm";
-import ListingsPage from "./components/ListingsPage";
-import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
   <>
-    <NavBar />
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/users" element={<UserForm />} />
-        <Route path="/listings" element={<ListingsForm />} />
-        <Route path="listingsPage" element={<ListingsPage />} />
-      </Routes>
-    </Router>
+    <App />
   </>,
   document.getElementById("root")
 );
